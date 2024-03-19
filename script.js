625a59498cee23dd2b29cbeb403dc575710a8701
MrAtayee <ibrahimatayee207@gmail.com>
2024-03-19 04:35:59 -0800

    ContextMenu

---
+++ script.js
@@ -0,0 +1,31 @@
+const contextmenu = document.querySelector('.container');
+const submenu = document.querySelector('.share-menu');
+
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    let windWidth = window.innerWidth;
+    let windHeight = window.innerHeight;
+    let cmWidght = contextmenu.offsetWidth;
+    let cmheight = contextmenu.offsetHeight;
+
+    x = x > windWidth - cmWidght ? windHeight - cmWidght : x;
+    y = y > windHeight - cmheight ? windHeight - cmheight : y;
+    
+    if (x > windWidth - cmWidght - submenu.offsetWidth) {
+        submenu.style.left = '-170px';
+    } else {
+        submenu.style.left = '260px';
+    }
+
+    contextmenu.style.left = `${x}px`;
+    contextmenu.style.top = `${y}px`;
+    contextmenu.style.visibility = 'visible';
+})
+
+
+window.addEventListener('click', () => {
+    contextmenu.style.visibility = 'hidden';
+})
